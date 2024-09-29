ebb921c119aae8b1547303201c7ea315b325dcbd
Gagandeep9090 <90343099+Gagandeep9090@users.noreply.github.com>
2024-09-29 16:16:30 -0400

    removed unwanted code

--- lib/reports/repository/report_model.dart
+++
@@ -1,9 +0,0 @@
-class ReportModel {
-
-  final String title;
-  final String description;
-  final String date;
-  final String imageReport;
-
-  ReportModel(this.title, this.description,this.date, this.imageReport);
-}
